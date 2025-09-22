b8e5a4215ec997f48cfdc5b6177e35cbf0529ef6
evrahimhasan <evrahimhasanemran.com>
2025-09-22 23:38:04 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Countries from './components/countries/countries'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
